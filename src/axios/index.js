e66ccddcfcb1847e0d09d194cc5101d5d65321c6
Subhrajyoti <subha.ionic3@gmail.com>
2020-11-27 17:52:17 +0530

    change axios's end-point

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&s=`,
+  baseURL: "https://flask-movie-app.herokuapp.com/movie/",
 });
 
 export default instance;
