e029de61647b8ad54239276418eac5dd8b167b52
Subhrajyoti <subha.ionic3@gmail.com>
2020-11-30 13:20:54 +0530

    change apiEnd point

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://flask-movie-app.herokuapp.com/movie/",
-  baseURL: "http://127.0.0.1:5000/",
+  baseURL: "https://flask-movie-app.herokuapp.com/",
 });
 
 export default instance;
