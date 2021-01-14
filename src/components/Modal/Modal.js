15ac390dc22bb675aaece61d15f99164f0688d1b
Subhrajyoti <subha.ionic3@gmail.com>
2021-01-15 01:40:18 +0530

    Small cosmetic changes

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Row, Col } from "react-bootstrap";
+
+import * as classes from "./Modal.module.css";
+import default_image from "../../assets/media/images/default_image.png";
 
 const CustomModal = ({ show, handleClose, ...props }) => {
   return (
@@ -7,7 +10,38 @@ const CustomModal = ({ show, handleClose, ...props }) => {
       <Modal.Header closeButton>
         <Modal.Title>{props.Title}</Modal.Title>
       </Modal.Header>
-      <Modal.Body>{props.Plot}</Modal.Body>
+      <Modal.Body>
+        <div>
+          <Row>
+            <Col sm={4}>
+              <div
+                className={classes.moviePoster}
+                style={{
+                  backgroundImage: `url(${
+                    props.Poster === "N/A" ? default_image : props.Poster
+                  })`,
+                }}
+              ></div>
+            </Col>
+            <Col sm={8} className={classes.moviePlotWrapper}>
+              <div>{props.Plot}</div>
+            </Col>
+          </Row>
+        </div>
+        <div className={classes.ratingsSection}>
+          <Row>
+            {props.Ratings.map(({ Source, Value }) => {
+              return (
+                <Col key={Source}>
+                  <p className={classes.movieRatings}>
+                    {Source}: {Value}
+                  </p>
+                </Col>
+              );
+            })}
+          </Row>
+        </div>
+      </Modal.Body>
       {/* <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
           Close
